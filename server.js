fc2c1ff8c6dc66dc699ccb3b9dc544397405788a
anujarajeev <anujarajeev95@gmail.com>
2020-06-25 21:20:22 +0530

    read and create  completed

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-console.log('May Node be with  ssh you')
-const express = require('express');
-const bodyParser= require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const MongoClient = require('mongodb').MongoClient
-const connectionString="mongodb+srv://admin:password323@cluster0-vbkw4.mongodb.net/assignment?retryWrites=true&w=majority"
+const MongoClient = require("mongodb").MongoClient;
+const connectionString =
+  "mongodb+srv://admin:password323@cluster0-vbkw4.mongodb.net/assignment?retryWrites=true&w=majority";
 
-app.use(bodyParser.urlencoded({ extended: true }))
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    const db = client.db("assignment");
+    console.log("Connected to Database", db);
+    const productsCollection = db.collection("products");
+    app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: true }));
+    //1
+    app.get("/", (req, res) => {
+      db.collection("products")
+        .find()
+        .toArray()
+        .then((results) => {
+          res.render("index.ejs", { products: results });
 
+          console.log(results);
+        })
+        .catch((error) => console.error(error));
+    });
+    //2
+    app.post("/products", (req, res) => {
+      productsCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-    // Note: __dirname is directory current directory you're in. Try logging it and see what you get!
-    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+      console.log("Hellooooooooooooooooo!", req.body);
+    });
+
+    app.listen(3000, function () {
+      console.log("listening on 3000");
+    });
   })
-
-  app.post('/quotes', (req, res) => {
-    console.log('Hellooooooooooooooooo!',req.body)
-  })
-
-  MongoClient.connect(connectionString,   {useUnifiedTopology: true},).then(client => {
-    const db = client.db("assignment")
-    console.log('Connected to Database',db)
-  })  .catch(error => console.error(error))
-
-  app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+  .catch((error) => console.error(error));
